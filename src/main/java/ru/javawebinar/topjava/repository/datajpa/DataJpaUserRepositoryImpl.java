9e60a446f4c7d4d041444fce9faaf4905f067690
ABaykov <alexandr.baykov@gmail.com>
2018-11-12 19:53:05 +0300

    6_06_HW5_optional_fetch_join

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
@@ -40,4 +40,9 @@ DataJpaUserRepositoryImpl implements UserRepository {
     public List<User> getAll() {
         return crudRepository.findAll(SORT_NAME_EMAIL);
     }
+
+    @Override
+    public User getWithMeals(int id) {
+        return crudRepository.getWithMeals(id);
+    }
 }
